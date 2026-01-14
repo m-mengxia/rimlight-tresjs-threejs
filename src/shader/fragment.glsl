b714b86a4a9b0dc4295f4642ccc87a3a13af47f7
mejohn <13913496+m-mengxia@users.noreply.github.com>
2026-01-14 15:10:53 +0800

    Update fragment.glsl

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -6,5 +6,13 @@ varying vec3 vNormal;
 varying vec3 vViewPosition;
 
 void main() {
-  
+  vec3 normal = normalize(vNormal);
+  vec3 viewDir = normalize(vViewPosition);
+
+  float rim = 1.0 - max(0.0, dot(normal, viewDir));
+  rim = pow(rim, rimPower) * rimIntensity;
+
+  vec3 finalColor = vec3(0.1, 0.1, 0.15) + rimColor * rim;
+
+  gl_FragColor = vec4(finalColor, 1.0);
 }
